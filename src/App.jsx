d748b5c3a73855c1e13785ca751ff6e718e14025
Sarah Hopp <sarahhopp717@gmail.com>
2026-03-10 17:14:39 -0700

    all finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
+import ShowSelection from "./shows/ShowSelection";
+import ShowDetails from "./shows/ShowDetails";
+import { useState } from "react";
+import { tvShows as showsData } from "./shows/data";
+
 /**
  * React TV is an web streaming platform that allows users to browse
  * through the episodes of a variety of different shows.
  */
 export default function App() {
+  const [shows] = useState(showsData);
+  const [selectedShow, setSelectedShow] = useState();
   return (
     <>
       <header>
         <p>React TV</p>
+        <ShowSelection shows={shows} setSelectedShow={setSelectedShow} />
       </header>
-      <main></main>
+      <main>
+        <ShowDetails show={selectedShow} />
+      </main>
     </>
   );
 }
